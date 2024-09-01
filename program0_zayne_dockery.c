6e2b68ab31e01f3b330d216384eca6cbb71c1330
Zayne <49252407+zaynedoc@users.noreply.github.com>
2024-09-01 14:57:13 +0000

    I forgot to commit

---
+++ program0_zayne_dockery.c
@@ -0,0 +1,25 @@
+//********************************************************
+// program-0.c
+// Author: Zayne Dockery
+// Class: COP 3223, Professor Parra
+// Purpose: This program prints a specific message to the
+// command line. The main purpose of this assignment is to
+// check and see whether I can connect to the Codespaces Server.
+// Input: None
+//
+// Output: (to the command line) A breif Message
+// //********************************************************
+
+
+// Header file for input output functions
+#include <stdio.h>
+ 
+// main function - where we will print the message
+int main()
+{
+ 
+    // prints the message to the command line.
+    printf("The current month is August.\nThe current year is 2024.\nI am a student at UCF.\nThis Fall 2024 I am taking Intro to Programming in C with Benji.\nI am super exicted to learn many things this semester!\n");
+ 
+    return 0;
+}
